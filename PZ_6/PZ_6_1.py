7cc4ad7e8ae54d6cf91534f7dcfa4913050a50e0
nastenka09 <achernenko_09@mail.ru>
2025-12-13 12:31:55 +0300

    Update PZ_6_1.py

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -2,3 +2,22 @@
 #Задание Дан список ненулевых целых чисел размера N. Проверить, чередуются ли в нем положительные и отрицательные числа.
  Если чередуются, то вывести 0, если нет, то вывести порядковый номер первого элемента, нарушающего закономерность.
 '''
+import random
+
+try:
+    N = random.randint(1, 10)
+    numbers = []
+    for _ in range(N):
+        num = random.randint(-100, 100)
+        if num != 0:
+            numbers.append(num)
+    result = 0
+    for i in range(1, N):
+        if numbers[i-1] * numbers[i] > 0:
+            result = i + 1  
+    
+    print(f"Сгенерированный список: {numbers}")
+    print(f"Результат: {result}")
+    
+except ValueError:
+    print("Ошибко")
