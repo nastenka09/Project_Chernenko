a224c22d9131ddc5c76d70f7dcaf2b9efc84cc04
nastenka09 <achernenko_09@mail.ru>
2025-12-13 12:11:56 +0300

    Update PZ_5_1.py

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -3,3 +3,18 @@
 в котором в первой строке 1 звездочка, во второй - 2, в третьей - 3, ...,
  в строке с номером m - m звездочек.
 '''
+try:
+    def PaintStars(m):
+        row = 1
+        while row <= m:
+            stars = 1
+            while stars <= row: 
+                print("*", end="")
+                stars += 1
+            print() 
+            row += 1
+
+    a = int(input('Введите количество строк: '))
+    PaintStars(a)
+except ValueError:
+    print('Ошибка! Введите число')
